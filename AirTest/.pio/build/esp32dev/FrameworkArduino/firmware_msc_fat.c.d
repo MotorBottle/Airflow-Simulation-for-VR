68f38081c3ff5afd83a357980b12afebeb1213e1
motorbottle <motorbottt@gmail.com>
2024-05-19 17:04:30 -0400

    update

---
+++ AirTest/.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.o: \
+ C:/Users/Ccrrq/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/Ccrrq/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
